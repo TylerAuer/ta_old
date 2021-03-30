149241c711bf87602172551ed721c3354afd2e14
Tyler Auer <Tyler.Auer@gmail.com>
2021-03-29 21:11:47 -0700

    Parse featured post data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,13 +5,28 @@ import { GlobalStyles } from '@/components/GlobalStyles';
 import { SetWidth } from '@/components/SetWidth';
 import { Header } from '@/components/Header';
 import { Footer } from '@/components/Footer';
+import { Chip } from '@/components/Chip';
+
+const FeaturedPost = ({ post }) => {
+  const { date_created, title, subtitle, category, tags, location } = post.frontmatter;
+  const { blog } = post.fields;
+
+  return (
+    <article>
+      <h4>{title}</h4>
+      <div>{subtitle}</div>
+      {location && <div>{location}</div>}
+      {tags.map((tag) => (
+        <Chip to={`/${blog}/tag/${tag.toLowerCase()}`}>{tag}</Chip>
+      ))}
+    </article>
+  );
+};
 
 const Home: React.FC<PageProps> = ({ data }) => {
   const posts = data.allMdx.nodes;
   const featuredCodePosts = posts.filter((post) => post.fields.blog === 'code');
-  const featuredAdventurePosts = posts.filter(
-    (post) => post.fields.blog === 'code',
-  );
+  const featuredAdventurePosts = posts.filter((post) => post.fields.blog === 'adventure');
 
   return (
     <GlobalStyles>
@@ -23,9 +38,8 @@ const Home: React.FC<PageProps> = ({ data }) => {
             <div>Teacher turned software developer</div>
 
             <p>
-              After teaching math for twelve years, I taught myself to code. Now
-              I build systems to help developers at{' '}
-              <a href="nextdoor.com">Nextdoor</a> move fast. I also enjoy
+              After teaching math for twelve years, I taught myself to code. Now I build systems to
+              help developers at <a href="nextdoor.com">Nextdoor</a> move fast. I also enjoy
               creating puzzles, games, and interactives.
             </p>
             <div>Two Thirds Truth and One Third Lies About Me</div>
@@ -41,44 +55,39 @@ const Home: React.FC<PageProps> = ({ data }) => {
             <h2>Projects</h2>
             <h3>Bad Calculators</h3>
             <p>
-              A collection of puzzles built from painfully bad calculators with
-              150k puzzle attempts by users.
+              A collection of puzzles built from painfully bad calculators with 150k puzzle attempts
+              by users.
             </p>
             <h3>Fruit Matrix</h3>
             <p>
-              Rate fruit based on tastiness and ease of eating then see how your
-              ratings compare to everyone else's with detailed analytics in this
-              full-stack app inspired by XKCD and the NYTimes.
+              Rate fruit based on tastiness and ease of eating then see how your ratings compare to
+              everyone else's with detailed analytics in this full-stack app inspired by XKCD and
+              the NYTimes.
             </p>
             <h3>Super Stupid Props Charity Game</h3>
             <p>
-              A Super Bowl props game where players optionally contribute to a
-              community pot and the winner selects a charity to donate the pot
-              to. In 2021 we donated $1,568 to the Pancreatic Cancer Action
-              Network! <Link to="/">Sign up</Link> to play next year; donations
-              optional.
+              A Super Bowl props game where players optionally contribute to a community pot and the
+              winner selects a charity to donate the pot to. In 2021 we donated $1,568 to the
+              Pancreatic Cancer Action Network! <Link to="/">Sign up</Link> to play next year;
+              donations optional.
             </p>
           </SetWidth>
         </section>
         <section id="blogs">
           <SetWidth>
             <h2>Writing</h2>
-            <article>
-              <h3>Code</h3>
-              <h4>Linked Lists</h4>
-              <p>Are they lists, really?</p>
-              <h4>What the heck is a $PATH</h4>
-              <p>I'm not sure I know.</p>
-              <h4>On changing careers</h4>
-              <p>If you want to, you should (with caveats).</p>
-            </article>
-            <article>
-              <h3>Adventure</h3>
-              <h4>Cody Peak Scramble</h4>
-              <p>More fun when the tram is running.</p>
-              <h4>Cody Peak Scramble</h4>
-              <p>More fun when the tram is running.</p>
-            </article>
+            <section>
+              <h3>Code Blog</h3>
+              {featuredCodePosts.map((post) => (
+                <FeaturedPost post={post} />
+              ))}
+            </section>
+            <section>
+              <h3>Adventure Blog</h3>
+              {featuredAdventurePosts.map((post) => (
+                <FeaturedPost post={post} />
+              ))}
+            </section>
           </SetWidth>
         </section>
       </main>
