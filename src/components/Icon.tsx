b0e0176777a042f32d8e5f94c90495492ba952d0
Tyler Auer <Tyler.Auer@gmail.com>
2021-05-07 21:21:03 -0700

    Add icons and styles to header

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,22 +1,27 @@
 import { SerializedStyles } from '@emotion/react';
+
 import calculator from '../img/icons/009-calculator.svg';
+import code from '../img/icons/015-coding.svg';
 import football from '../img/icons/010-american-football.svg';
 import fruit from '../img/icons/014-fruit.svg';
+import github from '../img/icons/005-github.svg';
 import mountains from '../img/icons/013-mountains.svg';
-import code from '../img/icons/015-coding.svg';
+import twitter from '../img/icons/003-twitter.svg';
 
 type Props = {
-  icon: 'calculator' | 'football' | 'fruit' | 'mountains' | 'code';
+  icon: 'calculator' | 'code' | 'football' | 'fruit' | 'github' | 'mountains' | 'twitter';
   sx?: SerializedStyles;
 };
 
 export const Icon: React.FC<Props> = ({ sx, icon }) => {
   const src = {
     calculator,
+    code,
     football,
     fruit,
+    github,
     mountains,
-    code,
+    twitter,
   };
 
   return <img css={sx} src={src[icon]} alt={icon} />;
