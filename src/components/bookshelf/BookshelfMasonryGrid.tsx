aee4e4b90e40d3e29e6bf257c1c3f36ca8dbd8ae
Tyler Auer <Tyler.Auer@gmail.com>
2021-05-11 21:45:59 -0700

    Build working masonry grid

--- src/components/bookshelf/BookshelfMasonryGrid.tsx
+++ src/components/bookshelf/BookshelfMasonryGrid.tsx
@@ -1,12 +1,19 @@
-import { GenreType } from '@/types';
+import { useState, useEffect, useLayoutEffect } from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
+import { css } from '@emotion/react';
 import { getImage, GatsbyImage } from 'gatsby-plugin-image';
+
+import { GenreType } from '@/types';
+
+type BookType = {};
 
 type Props = {
   activeFilters: GenreType[];
 };
 
 export const BookshelfMasonryGrid: React.FC<Props> = ({ activeFilters }) => {
+  const [loading, setLoading] = useState<Boolean>(true);
+
   const query = useStaticQuery(graphql`
     {
       allBookDataYaml {
@@ -14,9 +21,8 @@ export const BookshelfMasonryGrid: React.FC<Props> = ({ activeFilters }) => {
           cover {
             childImageSharp {
               gatsbyImageData(width: 300, placeholder: DOMINANT_COLOR)
-              original {
-                height
-                width
+              resize {
+                aspectRatio
               }
             }
             name
@@ -27,14 +33,71 @@ export const BookshelfMasonryGrid: React.FC<Props> = ({ activeFilters }) => {
     }
   `);
 
+  const getShortestColumnIndex = (heights: number[]): number => {
+    let minIndex = -1;
+    let minHeightSeen = Infinity;
+
+    for (let i = 0; i < heights.length; i++) {
+      if (heights[i] < minHeightSeen) {
+        minHeightSeen = heights[i];
+        minIndex = i;
+      }
+    }
+
+    if (minIndex === -1 || minHeightSeen === Infinity) {
+      throw new Error('Invalid computation of shortest column height');
+    }
+
+    return minIndex;
+  };
+
   const books = query.allBookDataYaml.nodes;
+
+  useLayoutEffect(() => {
+    console.log('Layout effect reran');
+  }, [activeFilters]);
+
   console.log(books);
+  const gutterInPixels = 10;
+
+  // Will make these dynamic based on window size
+  const columnWidth = 300;
+  const columnCount = 4;
+
+  const columnHeights = new Array(columnCount).fill(0);
+  const columnsOfCovers = new Array(columnCount).fill(null).map(() => Array(0));
+
+  for (let book of books) {
+    const columnIndexForNextBook = getShortestColumnIndex(columnHeights);
+    console.log('colIdxToAddTo', columnIndexForNextBook);
+    console.log('heights', columnHeights);
+    console.log('covers', columnsOfCovers);
+    columnsOfCovers[columnIndexForNextBook].push(book);
+    columnHeights[columnIndexForNextBook] +=
+      Math.round(columnWidth / book.cover.childImageSharp.resize.aspectRatio) + gutterInPixels;
+  }
 
   return (
-    <div>
-      {books.map((b) => (
-        <GatsbyImage alt={b.title} image={getImage(b.cover)} />
+    <section
+      css={css`
+        display: flex;
+        align-items: flex-start;
+      `}
+    >
+      {columnsOfCovers.map((col, i) => (
+        <div
+          className={`column-${i}`}
+          css={css`
+            width: ${columnWidth}px;
+            display: inline-block;
+            margin: 0 ${gutterInPixels / 2}px;
+          `}
+        >
+          {col.map((book) => (
+            <GatsbyImage key={book.title} alt={book.title} image={getImage(book.cover)} />
+          ))}
+        </div>
       ))}
-    </div>
+    </section>
   );
 };
