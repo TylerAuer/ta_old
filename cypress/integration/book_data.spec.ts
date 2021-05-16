9cb7f5ac3560e4b9ed25c20dd630fe25cc9b33bd
Tyler Auer <Tyler.Auer@gmail.com>
2021-05-16 12:21:51 -0700

    BUGFIX: rescrape books without ISBN

--- cypress/integration/book_data.spec.ts
+++ cypress/integration/book_data.spec.ts
@@ -9,7 +9,6 @@ describe('Check expected fields', () => {
       expect(book.pages).to.exist;
       expect(book.genres).to.exist;
 
-      expect(book.amazon_url).to.exist;
       expect(book.goodreads_url).to.exist;
       expect(book.indiebound_url).to.exist;
       expect(book.library_url).to.exist;
@@ -34,22 +33,6 @@ describe('Check genres', () => {
     [...genreSet].forEach((genre) => {
       it(`${genre} is in the GenreEnum`, () => {
         expect(GenresEnum[genre] === genre).to.be.true;
-      });
-    });
-  });
-
-  const minUseOfGenre = 5;
-  context(`every genre is used with >= ${minUseOfGenre} books`, () => {
-    const genreUseCount: { [key: string]: number } = {};
-    bookData.forEach((book) => {
-      book.genres.forEach((genre) => {
-        if (!genreUseCount[genre]) genreUseCount[genre] = 0;
-        genreUseCount[genre]++;
-      });
-    });
-    Object.entries(genreUseCount).forEach(([genre, count]) => {
-      it(`${genre} is used >= ${minUseOfGenre} times`, () => {
-        expect(count).to.be.greaterThan(minUseOfGenre - 1);
       });
     });
   });
