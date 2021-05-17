b2e4e9af8b1571d407ba976e696a7d4fec79ba7a
Tyler Auer <Tyler.Auer@gmail.com>
2021-05-16 20:21:28 -0700

    Fix Cypress CI, try 1

---
+++ utils/fisher_yates_shuffle.ts
@@ -0,0 +1,20 @@
+// Shuffles list in place
+export function fisherYatesShuffle<T>(list: T[]): T[] {
+  let currentIndex = list.length;
+  let temporaryValue: any;
+  let randomIndex: number;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = list[currentIndex];
+    list[currentIndex] = list[randomIndex];
+    list[randomIndex] = temporaryValue;
+  }
+
+  return list;
+}
