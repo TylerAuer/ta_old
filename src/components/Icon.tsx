9bea4b158ca8b287fa527b3440e8cfd4ec7b094a
Tyler Auer <32402444+TylerAuer@users.noreply.github.com>
2021-05-16 17:10:22 -0700

    add project to index and include intro to bookshelf (#27)
    * Add bookshelf to projects
    
    * Add book link to header
    
    * Add book link to footer
    
    * Add intro to bookshelf
    
    * Extract icon nav to component

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -5,11 +5,22 @@ import fruit from '../img/icons/014-fruit.svg';
 import github from '../img/icons/005-github.svg';
 import mountains from '../img/icons/013-mountains.svg';
 import twitter from '../img/icons/003-twitter.svg';
+import book from '../img/icons/011-open-book.svg';
+import home from '../img/icons/008-home-icon-silhouette.svg';
 
 import { HtmlElementPropsType } from '@/types';
 
 type Props = {
-  icon: 'calculator' | 'code' | 'football' | 'fruit' | 'github' | 'mountains' | 'twitter';
+  icon:
+    | 'calculator'
+    | 'code'
+    | 'football'
+    | 'fruit'
+    | 'github'
+    | 'mountains'
+    | 'twitter'
+    | 'book'
+    | 'home';
 } & HtmlElementPropsType;
 
 export const Icon: React.FC<Props> = ({ sx, icon, id, className }) => {
@@ -21,6 +32,8 @@ export const Icon: React.FC<Props> = ({ sx, icon, id, className }) => {
     github,
     mountains,
     twitter,
+    book,
+    home,
   };
 
   return <img id={id} className={className} css={sx} src={src[icon]} alt={icon} />;
