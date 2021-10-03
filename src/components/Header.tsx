986083f87c7f1247cd2808fa2cd101068e2e8242
Tyler Auer <Tyler.Auer@gmail.com>
2021-10-03 12:41:12 -0400

    Organize Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,40 +1,20 @@
-import React from 'react';
 import { css } from '@emotion/react';
-
 import { Box } from '@/elements/Box';
-import { Icon } from '@/constants/Icon';
-import { Flex, FlexJustification } from '@/components/Flex';
 import { SPACING } from '@/constants';
 import { IconNavList } from './IconNavList';
 import { A, Heading } from '@/elements';
-
-// const Left: React.FC = () => (
-//   <A
-//     css={css`
-//       color: var(--color-punch);
-//       font-family: var(--font-special);
-//       font-size: 3rem;
-//       font-weight: 600;
-//     `}
-//     to="/"
-//   >
-//     <Icon
-//       icon="signature"
-//       sx={css`
-//         fill: var(--color-punch);
-//         width: 15rem;
-//         filter: drop-shadow(2px 2px var(--color-text-body));
-//         transition: all 0.2s;
-//       `}
-//     />
-//   </A>
-// );
 
 const CONTAINER_SX = css`
   display: flex;
   justify-content: space-between;
   align-items: center;
   margin: ${SPACING.xxxl} 0;
+`;
+
+const NAME_SX = css`
+  margin: 0;
+  line-height: 1.2;
+  width: auto;
 `;
 
 type PropsType = {
@@ -45,20 +25,14 @@ export function Header({ includeHomeLink = true }: PropsType) {
   return (
     <header>
       <Box sx={CONTAINER_SX}>
-        <Left />
+        <NameAndShortBio />
         <IconNavList includeHomeLink={includeHomeLink} />
       </Box>
     </header>
   );
 }
 
-const NAME_SX = css`
-  margin: 0;
-  line-height: 1.2;
-  width: auto;
-`;
-
-const Left = () => (
+const NameAndShortBio = () => (
   <div>
     <Heading sx={NAME_SX} level={1}>
       Tyler Auer
