74d5a313b4a36353647c750a02bf7b7ce51ea96c
Tyler Auer <Tyler.Auer@gmail.com>
2021-10-27 20:44:01 -0600

    Clean up

--- src/hooks/useLocalState.tsx
+++ src/hooks/useLocalState.tsx
@@ -1,13 +1,14 @@
 import { useState } from 'react';
 
 type SetLocalValueInput<T> = T | ((prev: T) => T);
+type SetLocalStateFunctionType<T> = (input: SetLocalValueInput<T>) => void;
 
 // Works just like useState, but keeps a copy of the state in local storage and tries to use
 // the value in local storage if it exists when initialized
 export function useLocalState<T>(
   key: string,
   initialValueIfNotFoundLocally: T,
-): [T, (SetLocalValueInput) => void] {
+): [T, SetLocalStateFunctionType<T>] {
   const [value, setValue] = useState(() => {
     const localStorateValue = localStorage.getItem(key);
     return localStorateValue !== null
@@ -15,7 +16,7 @@ export function useLocalState<T>(
       : initialValueIfNotFoundLocally;
   });
 
-  const setLocalValue = (input: SetLocalValueInput<T>): void => {
+  const setLocalValue: SetLocalStateFunctionType<T> = (input: SetLocalValueInput<T>) => {
     if (input instanceof Function) {
       const computedNewValue = input(value);
       localStorage.setItem(key, JSON.stringify(computedNewValue));
